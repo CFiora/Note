50c7763c0f112593ec3ba16344544283803ad7a1
fiora <fiora@github.com>
2020-11-29 14:39:44 +0800

    excel 2 db

--- note2/src/main/java/com/fiora/note2/util/FastExcelUtil.java
+++ note2/src/main/java/com/fiora/note2/util/FastExcelUtil.java
@@ -14,13 +14,17 @@ import java.util.List;
 
 public class FastExcelUtil {
     private static String filePathPrefix = "D:\\1500T\\";
-    private static String file = "音乐0.xlsx";//"图书0.xlsx","音乐0.xlsx"
+    private static String[] files = {"2018更新资源0.xlsx", "2019更新资源0.xlsx", "2020更新资源0.xlsx", "KTV0.xlsx", "百家0.xlsx", "电视剧0.xlsx",
+            "电影0.xlsx", "动漫0.xlsx", "高清经典0.xlsx", "更新资源0.xlsx", "纪录片0.xlsx",
+            "教育0.xlsx", "蓝光专区0.xlsx", "体育0.xlsx", "图书0.xlsx", "音乐0.xlsx",
+            "游戏IT0.xlsx", "中美百万0.xlsx", "综艺0.xlsx"};
+    //    private static String file = "音乐0.xlsx";//"图书0.xlsx","音乐0.xlsx"
     private static String Url = "jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";//参数参考MySql连接数据库常用参数及代码示例
     private static String name = "root";//数据库用户名
     private static String psd = "root";//数据库密码
     private static String jdbcName = "com.mysql.cj.jdbc.Driver";//连接MySql数据库
     private static String sql = "insert into net_disk_1(`code`,`name`,`path`,`size`,`time`,`md5`,`directory`) values(?,?,?,?,?,?,'0')";
-    private static int batchSize = 15000;
+    private static int batchSize = 10000;
 
     public static void main(String[] args) {
         bigExcel2DB();
@@ -28,38 +32,43 @@ public class FastExcelUtil {
 
     private static void bigExcel2DB() {
         try {
+            int total = 0;
             Class.forName(jdbcName);//向DriverManager注册自己
             Connection con = DriverManager.getConnection(Url, name, psd);//与数据库建立连接
             con.setAutoCommit(false);
             PreparedStatement pst = con.prepareStatement(sql);
-            try (InputStream is = new FileInputStream(filePathPrefix + file); ReadableWorkbook wb = new ReadableWorkbook(is)) {
-                Sheet sheet = wb.getFirstSheet();
-                List<Row> rows = sheet.read();
-                for (int i = 2; i < rows.size(); i++) {
-//                for (int i = 2; i < 30050; i++) {
-                    System.out.println("第" + (i + 1) + "行记录");
-                    Row row = rows.get(i);
-                    pst.setString(1, row.getCell(0).getText());
-                    pst.setString(2, row.getCell(1).getText());
-                    pst.setString(3, row.getCell(2).getText());
-                    pst.setString(4, row.getCell(3).getText());
-                    pst.setString(5, row.getCell(4).getText());
-                    pst.setString(6, row.getCell(5).getText());
-                    pst.addBatch();
-                    if (i % batchSize == 0) {//可以设置不同的大小；如50，100，500，1000等等
+            for (String file : files) {
+                try (InputStream is = new FileInputStream(filePathPrefix + file); ReadableWorkbook wb = new ReadableWorkbook(is)) {
+                    Sheet sheet = wb.getFirstSheet();
+                    List<Row> rows = sheet.read();
+                    for (int i = 2; i < rows.size(); i++) {
+//                    for (int i = 2; i < 3050; i++) {
+                        total++;
+                        System.out.println("文件" + file + "第" + (i + 1) + "行记录,总数第" + total + "行记录");
+                        Row row = rows.get(i);
+                        pst.setString(1, row.getCell(0) == null ? "" : row.getCell(0).getText());
+                        pst.setString(2, row.getCell(1) == null ? "" : row.getCell(1).getText());
+                        pst.setString(3, row.getCell(2) == null ? "" : row.getCell(2).getText());
+                        pst.setString(4, row.getCell(3) == null ? "" : row.getCell(3).getText());
+                        pst.setString(5, row.getCell(4) == null ? "" : row.getCell(4).getText());
+                        pst.setString(6, row.getCell(5) == null ? "" : row.getCell(5).getText());
+                        pst.addBatch();
+                        if (i % batchSize == 0) {//可以设置不同的大小；如50，100，500，1000等等
+                            pst.executeBatch();
+                            con.commit();
+                            pst.clearBatch();
+                        }
+                    }
+                    if (rows.size() % batchSize != 0) {//可以设置不同的大小；如50，100，500，1000等等
                         pst.executeBatch();
                         con.commit();
                         pst.clearBatch();
                     }
                 }
-                if (rows.size() % batchSize != 0) {//可以设置不同的大小；如50，100，500，1000等等
-                    pst.executeBatch();
-                    con.commit();
-                    pst.clearBatch();
-                }
-                pst.close();
-                con.close();
             }
+
+            pst.close();
+            con.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -83,9 +92,9 @@ public class FastExcelUtil {
             printOut(row2);
             Row row3 = rows.get(3);
             printOut(row3);
-            Row rowl2 = rows.get(rows.size()-2);
+            Row rowl2 = rows.get(rows.size() - 2);
             printOut(rowl2);
-            Row rowl1 = rows.get(rows.size()-1);
+            Row rowl1 = rows.get(rows.size() - 1);
             printOut(rowl1);
 
 //            try (Stream<Row> rows = sheet.openStream()) {
@@ -99,6 +108,7 @@ public class FastExcelUtil {
             e.printStackTrace();
         }
     }
+
     private static void printOut(Row row) {
         row.forEach(cell -> {
             System.out.print(cell.getText() + "\t");
