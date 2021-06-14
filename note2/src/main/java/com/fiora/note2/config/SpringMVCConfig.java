7de0eae7894e9b06125184950f7dc020ce10d025
fiora <fiora@github.com>
2021-06-14 22:32:33 +0800

    增加缓存

--- note2/src/main/java/com/fiora/note2/config/SpringMVCConfig.java
+++ note2/src/main/java/com/fiora/note2/config/SpringMVCConfig.java
@@ -1,12 +1,19 @@
 package com.fiora.note2.config;
 
 import com.alicloud.openservices.tablestore.SyncClient;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiora.note2.model.Token;
 import com.fiora.note2.service.TokenService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Slf4j
@@ -32,4 +39,26 @@ public class SpringMVCConfig {
         return new SyncClient(tsConfig.getEndPoint(), tsConfig.getAccessKeyId(), tsConfig.getAccessKeySecret(), tsConfig.getInstanceName());
     }
 
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
 }
--- note2/src/main/java/com/fiora/note2/controller/TableStoreController.java
+++ note2/src/main/java/com/fiora/note2/controller/TableStoreController.java
@@ -6,11 +6,13 @@ import com.fiora.note2.model.NetDisk;
 import com.fiora.note2.model.Token;
 import com.fiora.note2.service.TableStoreService;
 import com.fiora.note2.service.TokenService;
+import com.fiora.note2.util.RedisUtil;
 import com.fiora.note2.util.Result;
 import com.fiora.note2.vo.Page;
 import com.fiora.note2.vo.TableStoreRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class TableStoreController {
     private TokenService tokenService;
     @Autowired
     private TableStoreService tableStoreService;
+    @Autowired
+    private RedisUtil redisUtil;
     public static final int pageNum = 1;
     public static final int pageSize = 10;
 
@@ -39,8 +43,23 @@ public class TableStoreController {
         if(request.getPageSize() == null) {
             request.setPageSize(pageSize);
         }
+        String redisKey = getRedisKey(request);
+        if(redisUtil.hasKey(redisKey)) {
+            Object data = redisUtil.get(redisKey);
+            log.info("Data from redis -> " + data);
+            return Result.ok(data);
+        }
         Page<NetDisk> page = tableStoreService.search(request.getMatch(), request.getFilter(), (request.getPageNum()-1) * request.getPageSize(), request.getPageSize());
         Object result = JSON.toJSON(page);
+        log.info("Request table store -> " + result);
+        redisUtil.set(redisKey, result, 60*60*24*30);
         return Result.ok(result);
+    }
+
+    private String getRedisKey(TableStoreRequest request) {
+        if (ObjectUtils.isEmpty(request.getFilter())) {
+            request.setFilter("");
+        }
+        return request.getMatch()+"@@"+request.getFilter()+"@@"+request.getPageNum()+"@@"+request.getPageSize();
     }
 }
