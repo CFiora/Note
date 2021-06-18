209ace9450beceb989df21d3c153905fd82fe40c
CFiora <935144328@qq.com>
2021-06-18 15:49:43 +0800

    Update User.java

--- note2/src/main/java/com/fiora/note2/model/User.java
+++ note2/src/main/java/com/fiora/note2/model/User.java
@@ -6,12 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
-public class User {
+public class User implements Serializable {
     @Id
-    @GeneratedValue( strategy = GenerationType.AUTO)
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String pass;
