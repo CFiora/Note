2653da0020659130c71677a8876d363ea6575f77
CFiora <935144328@qq.com>
2021-06-18 15:48:15 +0800

    Update EmailUser.java

--- note2/src/main/java/com/fiora/note2/model/EmailUser.java
+++ note2/src/main/java/com/fiora/note2/model/EmailUser.java
@@ -6,12 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
-public class EmailUser {
+public class EmailUser implements Serializable {
     @Id
-    @GeneratedValue( strategy = GenerationType.AUTO)
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     private Long emailId;
     private Long userId;
